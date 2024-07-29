df274566843d00386b921f15170852e5db70c775
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-29 20:48:47 +0800

    v1.466 commit n push

--- app/src/main/java/com/example/a20240729teslachargerapplicationdesign/MainActivity.java
+++ app/src/main/java/com/example/a20240729teslachargerapplicationdesign/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
        {
            public void onClick(View v)
            {
-               Intent intent=new Intent(MainActivity.this,MidCharger.class);
+               Intent intent=new Intent(MainActivity.this,SouthCharger.class);
                startActivity(intent);
 
            }
