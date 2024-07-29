60d94e931c70c2b052d9a15abae59d8f6af316b0
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-29 19:45:16 +0800

    v1.252 commit n push

--- app/src/main/java/com/example/a20240729teslachargerapplicationdesign/NorthCharger.java
+++ app/src/main/java/com/example/a20240729teslachargerapplicationdesign/NorthCharger.java
@@ -1,5 +1,7 @@
 package com.example.a20240729teslachargerapplicationdesign;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,8 +35,48 @@ public class NorthCharger extends AppCompatActivity {
             }
         });
     }
-
-
+    public void KeelungTeslaSuperCharger(View view)
+    {
+        Intent i =new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:25.127532429076116, 121.73952648232363?q=tesla supercharger"));
+        startActivity(i);
+    }
+    public void TaipeiTeslaSuperCharger(View view)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:25.032208787619034, 121.55759693563817?q=tesla supercharger"));
+        startActivity(i);
+    }
+    public void NewTaipeiTeslaSuperCharger(View view)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:25.06219406877244, 121.45996660287139?q=tesla supercharger"));
+        startActivity(i);
+    }
+    public void YilanTeslaSuperCharger(View view)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:24.589788198852663, 121.65934561511496?q=tesla supercharger"));
+        startActivity(i);
+    }
+    public void TaoyuanTeslaSuperCharger(View view)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:24.994201823608147, 121.29724783477648?q=tesla supercharger"));
+        startActivity(i);
+    }
+    public void HsinchuCityTeslaSuperCharger(View view)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:24.813280310917975, 120.96805406827507?q=tesla supercharger"));
+        startActivity(i);
+    }
+    public void HisnchuCountryTeslaSuperCharger(View view)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:24.692538981321082, 121.139235980007?q=tesla supercharger"));
+        startActivity(i);
+    }
 
 
 }
